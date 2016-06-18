d9c73c89ef007def22f5aae0a2456c54a0afccc4
Paul <paulst30@googlemail.com>
2016-06-18 23:01:57 +0100

    Add files via upload

---
+++ bash/webscan.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+
+# Check for Root
+function rootcheck() {
+if [[ $USER != "root" ]] ; then
+echo "Please Note: This script must be run as root!"
+exit 1
+fi
+echo -e " Checking for Root or Sudo: ${g}PASSED!${endc}"
+}
+rootcheck
+HOST=$1
+echo "=================================================="
+echo "=== Scanning $HOST. Please wait...======="
+echo "=================================================="
+sleep 2
+nmap -sS $HOST 
+sleep 2
+echo "   "
+echo "$(tput setaf 3)[+]$(tput sgr0) Now scanning port 80 for any web directories"
+sleep 2
+dirb http://$HOST
+echo "     "
+echo "$(tput setaf 2)[+]$(tput sgr0) Scan Complete!"
